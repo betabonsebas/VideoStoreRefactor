cda0d76e6195434911f2c720932efc7f494f5850
Sebastian Bonilla <sebastian.bonilla@globant.com>
2019-07-13 00:27:22 -0500

    delete switch statement by polymorphism

--- VideoStore/Movie.swift
+++ VideoStore/Movie.swift
@@ -8,57 +8,9 @@
 
 import Foundation
 
-enum MovieType {
-    case childrens, regular, newRelease
-}
-
-class Movie {
-    private let title: String
-    private var priceCode: MovieType
+protocol Movie {
+    var title: String { get }
     
-    init(title: String, priceCode: MovieType) {
-        self.title = title
-        self.priceCode = priceCode
-    }
-    
-    var getPriceCode: MovieType {
-        return priceCode
-    }
-    
-    func setPriceCode(arg: MovieType) {
-        self.priceCode = arg
-    }
-    
-    var getTitle: String {
-        return title
-    }
-    
-    func determineAmount(daysRented: Int) -> Double {
-        var rentalAmount: Double = 0
-        
-        switch getPriceCode {
-        case .regular:
-            rentalAmount += 2
-            if daysRented > 2 {
-                rentalAmount += Double(daysRented - 2) * 1.5
-            }
-        case .childrens:
-            rentalAmount += 1.5
-            if daysRented > 3 {
-                rentalAmount += Double(daysRented - 3) * 1.5
-            }
-        case .newRelease:
-            rentalAmount += Double(daysRented * 3)
-        }
-        return rentalAmount
-    }
-    
-    func determineFrequentRenterPoints(daysRented: Int) -> Int {
-        let bonusIsEarned = (getPriceCode == MovieType.newRelease && daysRented > 1)
-        if bonusIsEarned {
-            return 2
-        }
-        return 1
-    }
-    
+    func determineAmount(daysRented: Int) -> Double
+    func determineFrequentRenterPoints(daysRented: Int) -> Int
 }
